5b8978fe0c9d81903941b75d37d746085c7a42c8
tuqi.stjj <situjunjie@come-future.com>
2022-03-19 00:02:27 +0800

    服务端启动成功

--- chat-server/src/main/java/org/situjunjie/chatserver/ChatServerApplication.java
+++ chat-server/src/main/java/org/situjunjie/chatserver/ChatServerApplication.java
@@ -1,13 +1,28 @@
 package org.situjunjie.chatserver;
 
+import org.situjunjie.chatserver.server.SimpleChatServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ChatServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChatServerApplication.class, args);
+
+        ApplicationContext applicationContext = SpringApplication.run(ChatServerApplication.class, args);
+
+        runSimpleChatServer(applicationContext);
+    }
+
+    /**
+     * 开启简单的IM服务端
+     * @param applicationContext
+     */
+    private static void runSimpleChatServer(ApplicationContext applicationContext) {
+        SimpleChatServer server = applicationContext.getBean(SimpleChatServer.class);
+        server.run();
     }
 
 }
---
+++ chat-server/src/main/java/org/situjunjie/chatserver/server/SimpleChatServer.java
@@ -0,0 +1,70 @@
+package org.situjunjie.chatserver.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+import org.situjunjie.chatserver.handler.EchoHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @className: SimpleChatServer
+ * @description: 简单的聊天室服务器
+ * @author: situjunjie
+ * @date: 2022/3/18
+ **/
+@Service("simpleChatServer")
+@Slf4j
+public class SimpleChatServer {
+
+    @Value("${server.port}")
+    private Integer port;
+
+    private EventLoopGroup bossGroup;
+
+    private EventLoopGroup workerGroup;
+
+    @Autowired
+    private EchoHandler echoHandler;
+
+
+
+    public void run() {
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bossGroup = new NioEventLoopGroup();
+        workerGroup = new NioEventLoopGroup();
+        bootstrap.channel(NioServerSocketChannel.class)
+                .group(bossGroup,workerGroup)
+                .option(ChannelOption.SO_KEEPALIVE,true)
+                .option(ChannelOption.SO_BACKLOG,128)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(echoHandler);
+                    }
+                });
+
+        try {
+            ChannelFuture channelFuture = bootstrap.bind(new InetSocketAddress(port)).sync();
+                if(channelFuture.isSuccess()){
+                    log.info("服务器监听端口{}成功",port);
+                }
+                channelFuture.channel().closeFuture().sync();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
